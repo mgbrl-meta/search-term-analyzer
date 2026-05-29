5edc42ff35537fd41d6c178276a9657ffe1d28ec
Maulik Gupta <Maulik@Brillares-MacBook-Air.local>
2026-05-29 18:05:50 +0530

    Remove orphaned dashboard components referencing deleted lib/api

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"0JHr2IJmt0Hf0jmoAUZumLXYr0261l5a2eM7eQlexFk=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"FRpHKntELbiUJaypepKDRoi/aKIHkIRVt8fJYd3DBPo=\"\n}"
